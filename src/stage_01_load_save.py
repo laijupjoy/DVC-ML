5650e9b1d4c7a2d690837055e5a5a3135a7d447a
LAIJU <laijupjoy01@gmail.comgit config --global user.name laijupjoygit config --global user.email laijupjoy01@gmail.com>
2022-01-06 14:01:48 +0530

    stage_02 added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -25,12 +25,11 @@ def get_data(config_path):
     df.to_csv(raw_local_file_path, sep=",", index=False)
 
 
-
-
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument("--config", "-c", default="config/config.yaml")
 
     parsed_args = args.parse_args()
+
     get_data(config_path=parsed_args.config)
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import json
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
@@ -11,3 +12,7 @@ def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
         print(f"directory is created at {dir_path}")
+
+def save_local_df(data, data_path, index_status=False):
+    data.to_csv(data_path, index=index_status)
+    print(f"data is saved at {data_path}")
